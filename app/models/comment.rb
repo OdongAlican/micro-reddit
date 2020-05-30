8d5bac59542ec700bcc069abfcb9976642c4e943
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-05-30 17:01:36 +0300

    Add stickler, linter files and fix errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :author_name, presence: true,
-             length: {minimum: 3, maximum: 15} 
-  validates :body, presence: true,  
-               uniqueness: true
+                          length: { minimum: 3, maximum: 15 }
+  validates :body, presence: true,
+                   uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    validates :name, presence: true, 
-               length: {maximum: 20}, 
-               uniqueness: true
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true,
+                   length: { maximum: 20 },
+                   uniqueness: true
 end
