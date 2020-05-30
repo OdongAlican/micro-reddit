cb426510fd92d9b641aeeb9a0c676138c15ff340
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-05-30 15:48:10 +0300

    Validate post and user tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, 
+               length: {maximum: 10}, 
+               uniqueness: true
+  validates :body, presence: true,  
+               uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    validates :name, presence: true, 
+               length: {maximum: 20}, 
+               uniqueness: true
 end
